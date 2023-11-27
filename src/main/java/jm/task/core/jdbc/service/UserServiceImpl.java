b73e335b88239f5cc8afeb7fe4ebe88ab44012e1
Adept <adeptiumion@gmail.com>
2023-11-27 16:46:18 +0700

    LastHibernateFix

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package jm.task.core.jdbc.service;
 
 import jm.task.core.jdbc.dao.UserDao;
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
@@ -11,7 +12,7 @@ public class UserServiceImpl implements UserService {
 
     // Хочу логировать вне зависимости от реализации UserDao. ↓
     private static final Logger log = Logger.getLogger(UserDaoJDBCImpl.class.getName());
-    private final UserDao userDao = new UserDaoJDBCImpl();
+    private final UserDao userDao = new UserDaoHibernateImpl();
 
     @Override
     public void createUsersTable() {
